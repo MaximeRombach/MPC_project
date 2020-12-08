f2af849becc456a40fa2124ac8742b22a20a877f
jonathan-muheim <74509113+jonathan-muheim@users.noreply.github.com>
2020-12-08 15:23:42 +0100

    To do's 1 + 2

---
+++ jonathan/to_do_2_1.m
@@ -0,0 +1,16 @@
+%% To do 2.1
+quad = Quad();
+[xs,us] = quad.trim(); % Compute steady−state for which 0 = f(xs,us) 
+% trim computes the state and inputs xs and us
+
+sys = quad.linearize(xs, us); % Linearize the nonlinear model
+% linearize computes the jacobian (w.r.t x and u to compute the matrices A
+% and B of the linearized system x+ = Ax + Bu
+
+%% To do 2.2
+% change of input variable
+sys_transformed = sys * inv(quad.T); % New system is A * x + B * inv(T) * v
+
+%% To do 2.3
+% decomposition into 4 independant systems
+[sys_x, sys_y, sys_z, sys_yaw] = quad.decompose(sys, xs, us)
